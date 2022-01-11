7805d49e65ad2296de2b0906cf2b3ea89f0b37d1
OumaymaAB <OumaymaAB@users.noreply.github.com>
2022-01-11 18:31:52 +0000

    deploy: a12b11e20299a7c6ebe767b20e042bbe0f254437

---
+++ precache-manifest.8b008568f3c187f06efd296563c361c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9419cec5b46625d04b747fc3d20629f",
+    "url": "/OumaymaAB/whatsapp-mern/index.html"
+  },
+  {
+    "revision": "d858baa2c2e1f283c906",
+    "url": "/OumaymaAB/whatsapp-mern/static/css/main.bb15fcdf.chunk.css"
+  },
+  {
+    "revision": "4a8fad23fe444359d62a",
+    "url": "/OumaymaAB/whatsapp-mern/static/js/2.46889492.chunk.js"
+  },
+  {
+    "revision": "63bc7b3f7b4a3cd8f9b1",
+    "url": "/OumaymaAB/whatsapp-mern/static/js/3.068a5464.chunk.js"
+  },
+  {
+    "revision": "d858baa2c2e1f283c906",
+    "url": "/OumaymaAB/whatsapp-mern/static/js/main.76ee13c4.chunk.js"
+  },
+  {
+    "revision": "88f61efa785e547a7d65",
+    "url": "/OumaymaAB/whatsapp-mern/static/js/runtime~main.4ccd45b3.js"
+  }
+]);
--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCvQSGYPh5Z8wZLS7pnwx494CNDkYVZyz0",
-    authDomain: "whatsapp-mern-4d69b.firebaseapp.com",
-    projectId: "whatsapp-mern-4d69b",
-    storageBucket: "whatsapp-mern-4d69b.appspot.com",
-    messagingSenderId: "455535196780",
-    appId: "1:455535196780:web:2a028e183a985fe4adb649"
-  };
